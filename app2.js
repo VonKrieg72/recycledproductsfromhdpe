0f55f74cdb7d9c6bd84da3fc8f44b9930ad1015b
David W Stokes <fieldmarshalvonkriegenfaust@comcast.net>
2021-09-18 15:26:01 -0400

    new js2 file added

---
+++ app2.js
@@ -0,0 +1,6 @@
+let likeplasticjewlery = prompt('Do you like HDPE jewlery?')
+
+if (likeplasticjewlery == 'yes') {
+    document.write('<h3>We got that too.</h3>');
+    document.write('<img src="https://content.instructables.com/ORIG/FV1/1Q4B/IUSLOYWU/FV11Q4BIUSLOYWU.png?auto=webp&frame=1&fit=bounds&md=2ce0e94e2ce2ab89cf4009431b19f8ce"/>') 
+}
