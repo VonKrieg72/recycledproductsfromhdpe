293cadf5ac499f51c5ec4b34caa18d00d38f6689
David W Stokes <fieldmarshalvonkriegenfaust@comcast.net>
2021-09-21 08:15:02 -0400

    lab 7 updates

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-// let userName = 'David'
-// userName = 'Barry'
-// console.log(userName)
+function getUserName(){
+    let userName = prompt('Please Enter Your Name: ');
+    console.log(userName);
+    document.write('<h2>' + userName + '</h2>');
+    return userName; // -> 'BARRY'
+}
 
+function getUserAnswer(){
+    let userAnswer = prompt('Do you want to reduce plastic waste?');
+    console.log(userAnswer);
+    
+    if (userAnswer.toLowerCase() == 'yes' ) {
+       document.write('<h3>We are glad to have you here friend</h3>'); 
+    } else {
+        document.write('<h3>Go cry about climate change then, trendy.</h3>'); 
+    }
+}
 
-// userName = "Steven"
-// console.log(userName)
-
-let userName = prompt('Please Enter Your Name: ');
-console.log(userName);
-document.write('<h2>' + userName + '</h2>');
-
-let userAnswer = prompt('Do you want to reduce plastic waste?');
-console.log(userAnswer);
-
-if (userAnswer == 'yes' ) {
-   document.write('<h3>We are glad to have you here ' + userName +'</h3>'); 
-} else {
-    document.write('<h3>' + userName +', go cry about climate change then, trendy.</h3>'); 
+function getPlasticJewleryAnswer(){
+    let likeplasticjewlery = prompt('Do you like HDPE jewlery?')
+    let url ="https://content.instructables.com/ORIG/FV1/1Q4B/IUSLOYWU/FV11Q4BIUSLOYWU.png?auto=webp&frame=1&fit=bounds&md=2ce0e94e2ce2ab89cf4009431b19f8ce.jpeg"
+    if (likeplasticjewlery.toLowerCase == 'yes') {
+        document.write('<h3>We got that too.</h3>');
+        document.write('<img src="' +url+ '"/>') 
+    }
 }
