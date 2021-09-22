1c4d70b7c0151636afa528acbeda70d08a830e7f
David W Stokes <fieldmarshalvonkriegenfaust@comcast.net>
2021-09-22 16:15:27 -0400

    lab 8 updates

--- app.js
+++ app.js
@@ -24,3 +24,49 @@ function getPlasticJewleryAnswer(){
         document.write('<img src="' +url+ '"/>') 
     }
 }
+
+
+// Create a Number guessing game
+// Figure the number to be guessed as well as the range
+// How many chances do they get
+// Messages for right or wrong
+//     look for xx
+// reward for the right guess
+// Prompt needed for the user
+//     validate input before checking
+// need some counter
+
+function userGuessingGame() {
+    correctAnswer = 45;
+    userAttempts = 5;
+    for(let i = 0; i < userAttempts; i++){
+        let userGuess = prompt('Please Enter a Number 1-100');
+        while(userGuess < 1 || userGuess > 100){
+            userGuess = prompt('Wrong Range.  Please Enter a Number 1-100');   
+        }
+        if (userGuess == correctAnswer){
+            alert('You got it right!');
+            break;
+        }else if(userGuess < correctAnswer){
+           alert('Too Low.  Try Again.') 
+        }  else if (userGuess > correctAnswer) {
+            alert('Too High.  Try Again')
+        }else {
+           alert('Something Else Went Wrong'); 
+        }
+        console.log(i);
+        if(i == userAttempts - 1){
+           alert('You did not win.  Correct Answer is: ' + correctAnswer); 
+        }
+    }
+}
+
+userGuessingGame();
+
+function plasticJewlery(){
+    let numberOfPlasticJewlery = prompt('How many times do you want to see plastic jewlery?')
+    for(let i = 0; i < numberOfPlasticJewlery; i ++){
+        document.write('<img src="' + url + '" >')
+    }
+    
+}
